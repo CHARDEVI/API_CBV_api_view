584fe474b2821931bc8331edd4e93169399c9523
CHARDEVI <deviprasadisaka@gmail.com>
2023-06-09 19:19:37 +0530

    first commit

--- cherry/views.py
+++ cherry/views.py
@@ -13,6 +13,28 @@ class ProductCRUD(APIView):
         PQS=Product.objects.all()
         PJD=ProductSerializer(PQS,many=True)
         return Response(PJD.data)
+        
+    def post(self,request):
+        PMSD=ProductSerializer(data=request.data)
+        if PMSD.is_valid():
+            SPO=PMSD.save()
+            
+            return Response({'message':'Product is Created'})
+        else:
+            return Response({'Failed':'Product Creation is Failed'})
+        
+
+
+
+    def put(self,request):
+        id=request.data['id']
+        PO=Product.objects.get(id=id)
+        UPO=ProductSerializer(PO,data=request.data)
+        if UPO.is_valid():
+            UPO.save()
+            return Response({'message':'Product is Updated'})
+        else:
+            return Response({'Failed':'Product is not Updated'})
 
 
 
